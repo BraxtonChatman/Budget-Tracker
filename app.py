f2a25cb788f3448749fbf684df9d2df7520b533d
Braxton C <braxton.a.chatman@gmail.com>
2026-03-01 12:54:26 -0800

    Fix db path

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ from datetime import datetime
 import os
 
 app = Flask(__name__, instance_relative_config=True)
+
+os.makedirs(app.instance_path, exist_ok=True)
 db_path = os.path.join(app.instance_path, 'budget.db')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///budget.db' # f'sqlite:///{db_path}' # 'sqlite:///budget.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = os.environ.get('SECRET_KEY', 'devkey')
 
